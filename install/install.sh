fa7cb755957089feffa7ad0d21596b7dae561b2b
Lucas Russo <lerwys@gmail.com>
2016-02-18 10:09:10 -0500

    install/install.sh: add TOP IOC directory as CL option

--- install/install.sh
+++ install/install.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-TOP_DIR=..
 INSTALL_DIR=$1
+TOP_DIR=$2
 
 function usage {
-    echo "Usage: $0 <install_directory>"
+    echo "Usage: $0 <install_directory> <top IOC location>"
 }
 
 if [ -z "$INSTALL_DIR" ]; then
     echo "\"INSTALL_DIR\" variable unset."
+    usage
+    exit 1
+fi
+
+if [ -z "$TOP_DIR" ]; then
+    echo "\"TOP_DIR\" variable unset."
     usage
     exit 1
 fi
