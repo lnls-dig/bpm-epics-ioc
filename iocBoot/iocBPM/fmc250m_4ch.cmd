ea5be8f82bec39e2d230b768037e8fa4e27044fb
Lucas Russo <lerwys@gmail.com>
2016-10-05 14:52:18 -0300

    iocBoot/iocBPM/fmc250m_4ch.cmd: pass ADC channel to DBs

--- iocBoot/iocBPM/fmc250m_4ch.cmd
+++ iocBoot/iocBPM/fmc250m_4ch.cmd
@@ -1,6 +1,6 @@
 ##################### FMC250M_4CH Channels ##########################
 
-dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADDR=0,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
-dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADDR=1,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
-dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADDR=2,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
-dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADDR=3,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
+dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADC_CHAN=0, ADDR=0,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
+dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADC_CHAN=1, ADDR=1,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
+dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADC_CHAN=2, ADDR=2,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
+dbLoadRecords("${TOP}/BPMApp/Db/BPMFmc250m_4ch.template", "P=${EPICS_HOSTNAME}, PORT=$(BPM_NAME), ADC_CHAN=3, ADDR=3,  BPM_NUMBER=$(BPM_NUMBER), TIMEOUT=1")
