1ba34833df1dcc446197e9faaaf61338569c5a77
Lucas Russo <lerwys@gmail.com>
2017-02-08 10:22:50 -0200

    iocBoot/iocBPM: remove unused enableCallbacks script
    This is already done in initCommands script

--- iocBoot/iocBPM/enableCallbacks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-channel_names="A B C D"
-
-for i in $channel_names; do
-    caput ${EPICS_HOSTNAME}:BPM17:ADC_${i}:EnableCallbacks 1
-    caput ${EPICS_HOSTNAME}:BPM17:ADC_${i}_STATS:EnableCallbacks 1
-done
