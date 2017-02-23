dbdde98e7773d537ae66897ea76c6b38540cd33d
Lucas Russo <lerwys@gmail.com>
2017-02-23 14:34:11 -0300

    scripts: add script to turn PVs into Passive
    This helps debugging the IOC (ASYN_TRACE_FLOW)
    and the software HALCS (TRACE) in verbose mode.

---
+++ scripts/bpm_epics_scan_passive.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BPM_PREFIX=$1
+
+caput ${BPM_PREFIX}:DI-BPM:MONITUpdtAll.SCAN Passive
+caput ${BPM_PREFIX}:DI-BPM:ADCAD9510MuxStatus-RB.SCAN Passive
+caput ${BPM_PREFIX}:DI-BPM:ADCAD9510PllStatus-RB.SCAN Passive
+
+for i in $(seq 0 23); do 
+	caput ${BPM_PREFIX}:DI-BPM:TRIGGER${i}TrnCnt-RB.SCAN Passive 
+	caput ${BPM_PREFIX}:DI-BPM:TRIGGER${i}RcvCnt-RB.SCAN Passive 
+
+	caput ${BPM_PREFIX}:DI-BPM:TRIGGER_PM${i}TrnCnt-RB.SCAN Passive 
+	caput ${BPM_PREFIX}:DI-BPM:TRIGGER_PM${i}RcvCnt-RB.SCAN Passive 
+done
+
+for i in $(seq 0 3); do 
+	caput ${BPM_PREFIX}:DI-BPM:ADC${i}Temp-RB.SCAN Passive 
+	caput ${BPM_PREFIX}:DI-BPM:ADC${i}Temp-RB.SCAN Passive 
+done
