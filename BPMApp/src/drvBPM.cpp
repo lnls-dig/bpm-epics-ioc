2b1ec119b704b785dc1f0291cac966c5112bfc70
Lucas Russo <lerwys@gmail.com>
2017-02-24 18:34:45 -0300

    BPMApp: fix acqCompleted setting again
    This was fixed on devel branch, but
    was overwritten by the merge.

--- BPMApp/src/drvBPM.cpp
+++ BPMApp/src/drvBPM.cpp
@@ -1330,6 +1330,11 @@ void drvBPM::acqTask(int coreID, double pollTime, bool autoStart)
                 callParamCallbacks(coreID);
             }
 
+            /* We have consumed our data. This is important if we abort the next
+             * acquisition, as we can detect that the current acquisition is completed,
+             * which would be wrong */
+            acqCompleted = 0;
+
             /* Only wait for the startEvent if we are waiting for a
              * new acquisition */
             if (newAcq && !autoStartFirst) {
@@ -1498,11 +1503,6 @@ void drvBPM::acqTask(int coreID, double pollTime, bool autoStart)
 
             /* Calculate positions and call callbacks */
             computePositions(coreID, pArrayAllChannels, channel);
-
-            /* We have consumed our data. This is important if we abort the next
-             * acquisition, as we can detect that the current acquisition is completed,
-             * which would be wrong */
-            acqCompleted = 0;
         }
 
         /* Release buffer */
