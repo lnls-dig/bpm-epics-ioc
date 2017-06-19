ab89954edc13807fbc7075b9f5a286a1e59de411
Lucas Russo <lerwys@gmail.com>
2017-06-19 09:57:06 -0300

    {BPMRFFEApp,iocBoot}: fix undefined R prefix for RFFE autosave

--- iocBoot/iocBPMRFFE/st.cmd
+++ iocBoot/iocBPMRFFE/st.cmd
@@ -25,5 +25,5 @@ asynSetTraceIOMask("$(BPM_NAME)",0,0x2)
 iocInit()
 
 # save things every thirty seconds
-create_monitor_set("auto_settings.req", 30,"P=${P}")
+create_monitor_set("auto_settings.req", 30,"P=${P}, R=${R}")
 set_savefile_name("auto_settings.req", "auto_settings_${P}${R}.sav")
