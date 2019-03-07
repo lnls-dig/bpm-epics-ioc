582a48a97df9f3496ea20112eb3cd62e98ee059b
Lucas Russo <lerwys@gmail.com>
2019-03-07 10:23:42 -0300

    BPMApp: fix partial Q position calculation
    We were using wrong parameters for the Q position.

--- BPMApp/src/convert.cpp
+++ BPMApp/src/convert.cpp
@@ -308,6 +308,8 @@ void ABCDtoXYQSPartial(const ABCD_ROW *ABCD, XYQS_ROW *XYQS, K_FACTORS *K, POS_O
 
         uint32_t S_AC = A + C;
         uint32_t S_BD = B + D;
+        uint32_t S_AB = A + B;
+        uint32_t S_CD = C + D;
         uint32_t S = S_AC + S_BD;
 
         /* Now compute the positions according to the model.  As this is an
@@ -322,6 +324,12 @@ void ABCDtoXYQSPartial(const ABCD_ROW *ABCD, XYQS_ROW *XYQS, K_FACTORS *K, POS_O
 
         int shift_BD = 0;
         int InvS_BD = Reciprocal(S_BD << 1, shift_BD);
+
+        int shift_AB = 0;
+        int InvS_AB = Reciprocal(S_AB << 1, shift_AB);
+
+        int shift_CD = 0;
+        int InvS_CD = Reciprocal(S_CD << 1, shift_CD);
 
         /* KX and XY should be divided by 2 in partial difference-over-sum. Shift partial sums by 1 bit to implement it. */
         uint32_t partial_AC_pos_x = DeltaToPosition(k_factors.KX, A - C, InvS_AC, shift_AC);
@@ -348,7 +356,7 @@ void ABCDtoXYQSPartial(const ABCD_ROW *ABCD, XYQS_ROW *XYQS, K_FACTORS *K, POS_O
         /* We scale Q up quite a bit more so that we have access to as much
          * information as possible: the values can be quite small,
          * particulary after Q_0 correction. */
-        xyqs.Q = partial_AC_pos + partial_BD_pos - pos_offsets.QOFFSET;
+        xyqs.Q = partial_AB_pos_q + partial_CD_pos_q - pos_offsets.QOFFSET;
         xyqs.S = k_factors.KSUM * S;
     }
 }
