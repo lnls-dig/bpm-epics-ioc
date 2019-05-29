bb2493a439b4b76cb2ac8602f3c7c3b319516bdd
Lucas Russo <lerwys@gmail.com>
2019-05-29 17:30:09 -0300

    scripts: add switching desync. counter monitoring to passive script

--- scripts/bpm_epics_scan_passive.sh
+++ scripts/bpm_epics_scan_passive.sh
@@ -5,6 +5,7 @@ BPM_PREFIX=$1
 caput ${BPM_PREFIX}:MonitEnable-Sel No
 caput ${BPM_PREFIX}:ADCAD9510MuxStatus-RB.SCAN Passive
 caput ${BPM_PREFIX}:ADCAD9510PllStatus-Mon.SCAN Passive
+caput ${BPM_PREFIX}:SwTagDesyncCnt-Mon.SCAN Passive
 caput ${BPM_PREFIX}:TbtTagDesyncCnt-Mon.SCAN Passive
 
 for i in $(seq 0 23); do 
