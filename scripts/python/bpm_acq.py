368a64fe68b7bcd1322499760831d5134625bb69
Lucas Russo <lerwys@gmail.com>
2020-08-06 10:24:35 -0300

    scripts/python: add post-trigger/shots options to acquisition script

--- scripts/python/bpm_acq.py
+++ scripts/python/bpm_acq.py
@@ -9,6 +9,8 @@ parser.add_argument('prefix', type=str, help='EPICS PV prefix')
 parser.add_argument('nr_samples', type=int, help='Number of acquisition samples')
 parser.add_argument('acq_channel', type=str, help='Acquisition channel')
 parser.add_argument('--acq_trigger_type', type=str, help='Acquisition channel', default='Now')
+parser.add_argument('--nr_post_samples', type=int, help='Number of acquisition post-trigger acquisition samples', default=0)
+parser.add_argument('--nr_shots', type=int, help='Number of acquisition shots', default=1)
 
 args = parser.parse_args()
 
@@ -16,6 +18,8 @@ args = parser.parse_args()
 bpm = BPM(args.prefix, wait_for_connection=True)
 
 bpm.nr_samples_pre = args.nr_samples
+bpm.nr_samples_post = args.nr_post_samples
+bpm.nr_shots = args.nr_shots
 bpm.acq_channel = BPMEnums.ACQCHAN[args.acq_channel]
 
 # Acquistion type
