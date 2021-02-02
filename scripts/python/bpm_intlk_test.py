e682e3e85cb33c161a546841b3cf468dbc4cd577
Lucas Russo <lerwys@gmail.com>
2021-02-02 13:10:37 -0300

    scripts/python: use unbuffered stdout for responsiveness

--- scripts/python/bpm_intlk_test.py
+++ scripts/python/bpm_intlk_test.py
@@ -9,6 +9,26 @@ from textwrap import indent
 from enum import IntEnum
 import datetime
 import re
+import sys
+
+# from: https://stackoverflow.com/questions/107705/disable-output-buffering
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def writelines(self, datas):
+        self.stream.writelines(datas)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+# unbuffer stdout for responsive printing
+sys.stdout = Unbuffered(sys.stdout)
 
 # test parameters
 
